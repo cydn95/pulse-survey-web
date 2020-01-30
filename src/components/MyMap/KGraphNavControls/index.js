8149cea0b271d43992f247c415a5cab949f3130a
cydn95 <devstar.sky20@gmail.com>
2020-01-30 12:03:28 +0800

    fix my-map layout on mobile

--- src/components/MyMap/KGraphNavControls/index.js
+++ src/components/MyMap/KGraphNavControls/index.js
@@ -31,41 +31,35 @@ const KGraphNavControls = (props) => {
 
     return (
         <div className='mapButtonGroup'>
-            <Row>
-                <Col> 
-                    <a className="waves-effect waves-light btn btn-info active" 
-                        onClick={e => saveGraph(e)} disabled={saveLoading ? "disabled" : "" } >Save</a>&nbsp;&nbsp;
-                </Col>
-                <Col> 
-                    <ButtonDropdown isOpen={viewDropDownOpen} toggle={() => updateViewDisplay()} id='viewMode'>
-                        <DropdownToggle caret>
-                            {displayedViewMode}
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem header>Selected View</DropdownItem>
-                            <DropdownItem active={displayedViewMode==='Org/ Team/ SH'} onClick={(e) => { updateMap(['Org','Team'], 'viewMode') }}>Org/ Team/ SH</DropdownItem>
-                            <DropdownItem active={displayedViewMode==='SH only'} onClick={(e) => { updateMap([], 'viewMode') }}>SH Only</DropdownItem>
-                            <DropdownItem active={displayedViewMode==='Org/ SH'} onClick={(e) => { updateMap(['Org'], 'viewMode') }}>Org/ SH</DropdownItem>
-                            <DropdownItem active={displayedViewMode==='Team/ SH'} onClick={(e) => { updateMap(['Team'], 'viewMode') }}>Team/ SH</DropdownItem>
-                        </DropdownMenu>
-                    </ButtonDropdown>
-                </Col>
-                <Col>
-                    <ButtonGroup>
-                        <Button onClick={(e) => { updateMap(selectedLayout, 'layout') }} disabled={['Radial','Sequential'].includes(selectedLayout) && !enableLayout}>{selectedLayout}</Button>
-                        <ButtonDropdown isOpen={layoutDropDownOpen} toggle={(e) => updateLayoutDisplay(e)}>
-                            <DropdownToggle caret>
-                            </DropdownToggle>
-                            <DropdownMenu>
-                                <DropdownItem header>Layout Options</DropdownItem>
-                                <DropdownItem id='standard' active={selectedLayout === 'Standard'} onClick={(e) => { updateMap('Standard', 'layout') }}>Standard</DropdownItem>
-                                <DropdownItem id='radial' active={selectedLayout === 'Radial'} onClick={(e) => { updateMap('Radial', 'layout') }} disabled={!enableLayout}>Radial</DropdownItem>
-                                <DropdownItem id='sequential' active={selectedLayout === 'Sequential'} onClick={(e) => { updateMap('Sequential', 'layout') }} disabled={!enableLayout}>Sequential</DropdownItem>
-                            </DropdownMenu>
-                        </ButtonDropdown>
-                    </ButtonGroup>
-                </Col>
-            </Row>
+            <a className="waves-effect waves-light btn btn-info active" 
+                onClick={e => saveGraph(e)} disabled={saveLoading ? "disabled" : "" } >Save</a>&nbsp;&nbsp;
+            <ButtonDropdown isOpen={viewDropDownOpen} toggle={() => updateViewDisplay()} id='viewMode'>
+                <DropdownToggle caret>
+                    {displayedViewMode}
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem header>Selected View</DropdownItem>
+                    <DropdownItem active={displayedViewMode==='Org/ Team/ SH'} onClick={(e) => { updateMap(['Org','Team'], 'viewMode') }}>Org/ Team/ SH</DropdownItem>
+                    <DropdownItem active={displayedViewMode==='SH only'} onClick={(e) => { updateMap([], 'viewMode') }}>SH Only</DropdownItem>
+                    <DropdownItem active={displayedViewMode==='Org/ SH'} onClick={(e) => { updateMap(['Org'], 'viewMode') }}>Org/ SH</DropdownItem>
+                    <DropdownItem active={displayedViewMode==='Team/ SH'} onClick={(e) => { updateMap(['Team'], 'viewMode') }}>Team/ SH</DropdownItem>
+                </DropdownMenu>
+            </ButtonDropdown>
+            &nbsp;&nbsp;
+            <ButtonGroup>
+                <Button onClick={(e) => { updateMap(selectedLayout, 'layout') }} disabled={['Radial','Sequential'].includes(selectedLayout) && !enableLayout}>{selectedLayout}</Button>
+                <ButtonDropdown isOpen={layoutDropDownOpen} toggle={(e) => updateLayoutDisplay(e)}>
+                    <DropdownToggle caret>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem header>Layout Options</DropdownItem>
+                        <DropdownItem id='standard' active={selectedLayout === 'Standard'} onClick={(e) => { updateMap('Standard', 'layout') }}>Standard</DropdownItem>
+                        <DropdownItem id='radial' active={selectedLayout === 'Radial'} onClick={(e) => { updateMap('Radial', 'layout') }} disabled={!enableLayout}>Radial</DropdownItem>
+                        <DropdownItem id='sequential' active={selectedLayout === 'Sequential'} onClick={(e) => { updateMap('Sequential', 'layout') }} disabled={!enableLayout}>Sequential</DropdownItem>
+                    </DropdownMenu>
+                </ButtonDropdown>
+            </ButtonGroup>
+               
         </div>
     );
 }
