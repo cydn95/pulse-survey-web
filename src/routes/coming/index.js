b8e54b86ac2681dc87d9d97817a6590a28930849
cydn95 <devstar.sky20@gmail.com>
2020-02-28 11:56:26 +0545

    Updates

--- src/routes/coming/index.js
+++ src/routes/coming/index.js
@@ -1,6 +1,9 @@
-import React, { Component, Fragment } from "react";
+import React, {} from "react";
 
+import { NavLink } from "react-router-dom";
 import MessageBox from "Components/MessageBox";
+
+import styles from './styles.scss';
 
 function ComingSoon() {
   return (
@@ -10,6 +13,10 @@ function ComingSoon() {
       imageUrl="/assets/img/profile-pic-l-2.jpg"
     >
       We are currently working on a super exciting feature
+      <br />
+        <NavLink to={`/`} className={styles.link}>
+        Go to Homepage
+        </NavLink>
     </MessageBox>
   )
 }
