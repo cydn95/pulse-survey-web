6dd59c8475c1e76e3aafcabe01b2e98fd411efe4
cydn95 <devstar.sky20@gmail.com>
2020-02-28 14:17:42 +0545

    Refactor guest user components

--- src/routes/forgot-password/index.js
+++ src/routes/forgot-password/index.js
@@ -1,70 +1,40 @@
-import React, { Component, Fragment } from "react";
-import IntlMessages from "Util/IntlMessages";
-import { Row, Card, CardTitle, Form, Label, Input, Button } from "reactstrap";
+import React, {} from "react";
+import { Input, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 import classnames from "classnames";
 
-import { Colxx } from "Components/CustomBootstrap";
-
 import styles from './styles.scss';
+import LeftRightContainer from "../../components/LeftRightContainer";
 
-class ForgotPasswordLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "demo@gogo.com"
-    };
-  }
-
-  componentDidMount() {
-    document.body.classList.add("background");
-  }
-  componentWillUnmount() {
-    document.body.classList.remove("background");
-  }
-  render() {
-    return (
-      <div className="login-container">
-        <div className="login-container__left">
-          <div className="login-container__left__logo">
-            <img className="login-container__left__logo--img" src="/assets/img/login/collective-insight.png" alt="collective-insight" />
-            <h1 className="login-container__left__logo--title">Collective Insight</h1>
-            <p className="login-container__left__logo--description">
-              Pulse by ProjectAI helps project managers make<br />sense of people’s collective insights for better project outcomes.
-            </p>
-          </div>
-        </div>
-        <div className="login-container__right">
-          <div className="login-container__right--login-panel">
-            <h2 className="login-container__right--login-panel--title">Reset password</h2>
-            <p className="white">
-              Enter your username or email to reset your password.
-      You will receive an email with instructions on how to reset your password.
-              If you are experiencing problems resetting your password <a href='#' className={styles.link}>contact us</a> or <a className={styles.link} href='#'>send us an email</a>.
+function ForgotPasswordLayout() {
+  return (
+    <LeftRightContainer>
+      <h2>Reset password</h2>
+      <p className="white">
+        Enter your username or email to reset your password.
+You will receive an email with instructions on how to reset your password.
+            If you are experiencing problems resetting your password&nbsp;
+              <a href='#' className={styles.link}>contact us</a> or <a className={styles.link} href='#'>send us an email</a>.
               <br></br>
-              <br></br>
-              If you are not a member, please{" "}
-              <NavLink to={`/register`} className={classnames("white", styles.link)}>
-                register
+        <br></br>
+        If you are not a member, please{" "}
+        <NavLink to={`/register`} className={classnames("white", styles.link)}>
+          register
               </NavLink>
-            </p>
-            <span className="login-container__right--login-panel--field-label">USERNAME or EMAIL</span>
-            <Input
-              type="text"
-              className="login-container__right--login-panel--field-input round-text-field"
-              name="username_or_email"
-              onChange={e => console.log(e.target.value)}
-            />
-            <div className={styles.actions}>
-              <Button className={classnames("round-btn green", styles.button)} onClick={e => console.log('reset')}>Reset Password</Button>
-              <a href="/login" className={styles.link}>Sign In</a>
-            </div>
-          </div>
-        </div>
+      </p>
+      <label>USERNAME or EMAIL</label>
+      <Input
+        type="text"
+        name="username_or_email"
+        onChange={e => console.log(e.target.value)}
+      />
+      <div className={styles.actions}>
+        <Button className={classnames("round-btn green", styles.button)} onClick={e => console.log('reset')}>Reset Password</Button>
+        <a href="/login" className={styles.link}>Sign In</a>
       </div>
-    )
-  }
+    </LeftRightContainer>
+  )
 }
 
 export default ForgotPasswordLayout;
--- src/routes/register/index.js
+++ src/routes/register/index.js
@@ -1,100 +1,47 @@
-import React, { Component, Fragment } from "react";
-import IntlMessages from "Util/IntlMessages";
-import { Row, Card, CardTitle, Form, Label, Input, Button } from "reactstrap";
-import { NavLink } from "react-router-dom";
-
-import { Colxx } from "Components/CustomBootstrap";
-
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { registerUser } from "Redux/actions";
 
-class RegisterLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "demo@gogo.com",
-      password: "gogo123",
-      name: "Sarah Kortney"
-    };
-  }
-  onUserRegister() {
-    if (this.state.email !== "" && this.state.password !== "") {
+import { Input, Button } from "reactstrap";
+import LeftRightContainer from "../../components/LeftRightContainer";
+
+function RegisterLayout(props) {
+  const [email, setEmail] = useState("demo@gogo.com");
+  const [password, setPassword] = useState("gogo123");
+  const [username, setUserName] = useState("Sarah Kortney");
+
+  const onUserRegister = () => {
+    if (email !== "" && password !== "") {
       // This is for adding user to Firebase. Commented out for demo purpose.  
-      // this.props.registerUser(this.state, this.props.history);
-      this.props.history.push("/");
+      // props.registerUser(this.state, this.props.history);
+      props.history.push("/");
     }
   }
 
-  componentDidMount() {
-    document.body.classList.add("background");
-  }
-  componentWillUnmount() {
-    document.body.classList.remove("background");
-  }
-  render() {
-    return (
-      <Fragment>
-        <div className="fixed-background" />
-        <main>
-          <div className="container">
-            <Row className="h-100">
-              <Colxx xxs="12" md="10" className="mx-auto my-auto">
-                <Card className="auth-card">
-                  <div className="position-relative image-side ">
-                    <p className="text-white h2">MAGIC IS IN THE DETAILS</p>
-                    <p className="white">
-                      Please use this form to register. <br />
-                      If you are a member, please{" "}
-                      <NavLink to={`/login`} className="white">
-                        login
-                      </NavLink>
-                      .
-                    </p>
-                  </div>
-                  <div className="form-side">
-                    <NavLink to={`/`} className="white">
-                      <span className="logo-single" />
-                    </NavLink>
-                    <CardTitle className="mb-4">
-                      <IntlMessages id="user.register" />
-                    </CardTitle>
-                    <Form>
-                      <Label className="form-group has-float-label mb-4">
-                        <Input type="name" defaultValue={this.state.name} />
-                        <IntlMessages id="user.fullname" />
-                      </Label>
-                      <Label className="form-group has-float-label mb-4">
-                        <Input type="email" defaultValue={this.state.email} />
-                        <IntlMessages id="user.email" />
-                      </Label>
-                      <Label className="form-group has-float-label mb-4">
-                        <Input type="password" />
-                        <IntlMessages
-                          id="user.password"
-                          defaultValue={this.state.password}
-                        />
-                      </Label>
-                      <div className="d-flex justify-content-end align-items-center">
-                        <Button
-                          color="primary"
-                          className="btn-shadow"
-                          size="lg"
-                          onClick={() => this.onUserRegister()}
-                        >
-                          <IntlMessages id="user.register-button" />
-                        </Button>
-                      </div>
-                    </Form>
-                  </div>
-                </Card>
-              </Colxx>
-            </Row>
-          </div>
-        </main>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <LeftRightContainer>
+      <h2>Register</h2>
+      <label>USERNAME</label>
+      <Input
+        type="text"
+        value={username}
+        name="username"
+        onChange={e => setUserName(e.target.value)}
+      />
+      <label>PASSWORD</label>
+      <Input
+        type="password"
+        value={password}
+        name="password"
+        onChange={e => setPassword(e.target.value)}
+      />
+      <Button className="login-container__right--login-panel--submit round-btn green" onClick={e => onUserRegister()}>Register</Button>
+    </LeftRightContainer>
+  )
+
+};
+
 const mapStateToProps = ({ authUser }) => {
   const { user, loading } = authUser;
   return { user, loading };
@@ -105,4 +52,4 @@ export default connect(
   {
     registerUser
   }
-)(RegisterLayout);
+)(withRouter(RegisterLayout));
