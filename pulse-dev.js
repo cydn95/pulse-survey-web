10d1cf99ecb0f5e4544dd901cc6e4340a6229d41
cydn95 <devstar.sky20@gmail.com>
2020-06-08 15:23:04 -0400

    remove log

--- pulse-dev.js
+++ pulse-dev.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 3031;
 const app = express();
-console.log(port);
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
