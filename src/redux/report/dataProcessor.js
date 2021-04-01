dd451ce7cca32474412e7ad1ef93c8d628098ecc
cydn95 <devstar.sky20@gmail.com>
2021-04-01 06:25:22 -0400

    driver analysis x-axis label

--- src/redux/report/dataProcessor.js
+++ src/redux/report/dataProcessor.js
@@ -36,7 +36,8 @@ export const getResultForSHGroup = (shGroupList, result) => {
             cnt++;
             sum += data.integerValue;
             const dateStr = data.updated_at.split("-");
-            const dateKey = dateStr[0] + "-" + dateStr[1];  // Year - Month
+            // const dateKey = dateStr[0] + "-" + dateStr[1];  // Year - Month
+            const dateKey = MONTH[Number(dateStr[1]) - 1] + " " + dateStr[0];
             // const dateKey = dateStr[1];    // Only Month
 
             if (dateKey in trend) {
@@ -98,7 +99,8 @@ export const getResultForTeam = (teamList, result) => {
             sum += data.integerValue;
             // console.log(data.updated_at.split("-"));
             const dateStr = data.updated_at.split("-");
-            const dateKey = dateStr[0] + "-" + dateStr[1];  // Year - Month
+            // const dateKey = dateStr[0] + "-" + dateStr[1];  // Year - Month
+            const dateKey = MONTH[Number(dateStr[1]) - 1] + " " + dateStr[0];
 
             if (dateKey in trend) {
               trend[dateKey].push(data.integerValue);
@@ -159,7 +161,8 @@ export const getResultForOrganization = (organizationList, result) => {
             sum += data.integerValue;
             // console.log(data.updated_at.split("-"));
             const dateStr = data.updated_at.split("-");
-            const dateKey = dateStr[0] + "-" + dateStr[1];  // Year - Month
+            // const dateKey = dateStr[0] + "-" + dateStr[1];  // Year - Month
+            const dateKey = MONTH[Number(dateStr[1]) - 1] + " " + dateStr[0];
 
             if (dateKey in trend) {
               trend[dateKey].push(data.integerValue);
