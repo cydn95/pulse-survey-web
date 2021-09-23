5531ab6622aa6c8673bab15c1bbf144d30d71836
cydn95 <devstar.sky20@gmail.com>
2021-09-23 17:55:28 +0800

    build a reveal page for 404

--- src/components/report/NoDashboard/index.js
+++ src/components/report/NoDashboard/index.js
@@ -8,7 +8,7 @@ import styles from "./styles.scss";
  * 227: not enough threshcnt
  * 400: no data
  */
-const NoDashboard = ({ code, threshold = 3}) => {
+const NoDashboard = ({ code, threshold = 3 }) => {
   return (
     <Fragment>
       {(code.toString() === "400" || code.toString() === "227") && (
@@ -19,8 +19,8 @@ const NoDashboard = ({ code, threshold = 3}) => {
           />
           <p className={styles["no-dashboard-title"]}>Waiting for other users</p>
           <p className={styles["no-dashboard-text"]}>
-            Not enough users have responded to ensure<br/>
-            anonymity. The dashboards will be visible<br/>
+            Not enough users have responded to ensure<br />
+            anonymity. The dashboards will be visible<br />
             when at least {threshold} users have provided their responses.
           </p>
         </div>
@@ -33,13 +33,24 @@ const NoDashboard = ({ code, threshold = 3}) => {
           />
           <p className={styles["no-dashboard-title"]}>Tell us what you think!</p>
           <p className={styles["no-dashboard-text"]}>
-            To view the dashboards, please complete the questions<br/>
-            in the About Me section!<br/><br/>
+            To view the dashboards, please complete the questions<br />
+            in the About Me section!<br /><br />
             If you can't answer a question, please select "Skip".
           </p>
         </div>
       )}
-
+      {code.toString() === "404" && (
+        <div className={styles["no-dashboard-root"]}>
+          <img
+            src="/assets/img/report/not-configured.svg"
+            className={styles["no-dashboard-image"]}
+          />
+          <p className={styles["no-dashboard-title"]}>Coming Soon</p>
+          <p className={styles["no-dashboard-text"]}>
+            Pulse is being configured for this project.  Stay tuned!
+          </p>
+        </div>
+      )}
     </Fragment>
   );
 };
