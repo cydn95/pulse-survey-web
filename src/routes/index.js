175bb5b9b5c08665f9cd5239e25d5596a394fc7d
cydn95 <devstar.sky20@gmail.com>
2021-10-04 22:21:35 +0800

    fix save map error

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,7 @@ import AboutMeSurvey from "./about-me";
 import MyMap from "./mymap";
 import Dashboard from "./dashboard";
 import Settings from "./settings";
+import Administration from "./administration"
 import Help from "./help";
 import ComingSoon from "./coming";
 import ProjectNotFound from "./project-not-found";
@@ -106,6 +107,7 @@ const MainApp = ({ history, location, match, surveyId }) => {
               path={`${match.url}/my-project/:projectUserId`}
               component={ComingSoon}
             />
+            <Route path={`${match.url}/admin`} component={Administration} />
             <Route path={`${match.url}/help`} component={Help} />
             <Route path={`${match.url}/tour`} component={MobileTour} />
             <Route path={`${match.url}/guide`} component={Tooltip} />
