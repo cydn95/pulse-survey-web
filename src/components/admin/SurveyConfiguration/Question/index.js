414d0b35394eafd92660c3e7c7271d7322624758
cydn95 <devstar.sky20@gmail.com>
2021-10-18 17:23:03 +0800

    fix some errors

--- src/components/admin/SurveyConfiguration/Question/index.js
+++ src/components/admin/SurveyConfiguration/Question/index.js
@@ -4,7 +4,7 @@ import classnames from 'classnames'
 import Select from 'Components/Select'
 import Input from 'Components/Input'
 import styles from './styles.scss'
-import QMark from 'Assets/img/admin/qmark.svg'
+import QMark from 'Assets/img/admin/qmark.png'
 import DeleteIcon from 'Assets/img/admin/delete.svg'
 
 const Tag = styled.span`
