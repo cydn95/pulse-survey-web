7d540381ab6a88be5d341ab4f11f3211a8e99a1d
cydn95 <devstar.sky20@gmail.com>
2022-01-31 14:06:36 -0800

    add some test

--- src/components/__tests__/Donut.test.jsx
+++ src/components/__tests__/Donut.test.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Accordion from '../accordion'
+import Donut from '../Donut'
 import { render, screen } from '@testing-library/react'
 
 describe('work properly', () => {
     test('adds 1 + 2 to equal 3', () => {
         expect(1 + 2).toBe(3);
     });
-    test('accordion works correctly', () => {
-        render(<Accordion headerSelector={(d) => { return "Header" }} data={[1, 2, 3]} />)
-        expect(screen.getByText('Header')).toBeInTheDocument();
+    test('Donut works correctly', () => {
+        render(<Donut />)
+        expect(screen.getByText('Donut')).toBeInTheDocument();
     })
 })
 
